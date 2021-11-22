52ee869e1ad48502250d6ac317710084f7506363
MissKWG <keisan0ryoku@gmail.com>
2021-11-21 17:45:56 -0800

    Update main.js
    update js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function displayFahrenheitTemperature(event) {
 
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celciusTemperture * 9) / 5 + 32;
+  let fahrenheitTemperature = (celsiusTemperture * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
