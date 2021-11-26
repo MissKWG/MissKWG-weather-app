c957a3db9a3de592188e8004bef3d6b86bc357c6
MissKWG <keisan0ryoku@gmail.com>
2021-11-25 17:25:14 -0800

    Update main.js
    add html forecast to javascript

--- main.js
+++ main.js
@@ -15,6 +15,31 @@ function formatDate(timestamp) {
   return ` ${day} ${hours}:${minutes} ${amPm}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tue", "Wed"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+        <div><i class="fas fa-wind wind-icon"></i>☁️</div>
+        <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">50°</span>
+                <span class="weather-forecast-temperature-min">36°</span>
+        </div>
+    </div>
+              
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -82,3 +107,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 search("California");
+displayForecast();
